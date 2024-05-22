61772986045cb9cc45f14bf3b384488d1fb8affc
songsamuel <songsme2886@gmail.com>
2024-05-23 02:11:43 +0900

    연습한 내용

--- homework/songsamuel/backend/app.js
+++ homework/songsamuel/backend/app.js
@@ -3,19 +3,23 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('연습많이 살 길이다.')
+  res.send('연습만이 살 길이다.1')
 })
 
 app.get('/', (req,res) => {
-  res.send('연습만이 살길이다.')
+  res.send('연습만이 살길이다.2')
 })
 
 app.get('/', (req, res) => {
-    res.send('연습만이 살길이다.')
+    res.send('연습만이 살길이다.3')
 })
 
 app.get('/', (req, res) => {
-    res.send('연습만이 살길이다.')
+    res.send('연습만이 살길이다.4')
+})
+
+app.get('/', (req, res) => {
+    res.send('연습만이 살길이다.5')
 })
 
 app.listen(port, () => {
