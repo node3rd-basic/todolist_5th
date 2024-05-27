a7c8955f3cbfbde3affa4995382f9bbb76590eb9
songsamuel <songsme2886@gmail.com>
2024-05-27 16:17:03 +0900

    문제 수정 및 find 함수 연습 추가

--- homework/songsamuel/fn.js
+++ homework/songsamuel/fn.js
@@ -1,33 +1,81 @@
+
 //1. 인사말 출력 함수
 function greet() {
     console.log("안녕하세요!");
 }
 
+greet(); // "안녕하세요."
 
-//2. 고정된 값 반환 함수
-function returnFive() {
-    return 5;
+
+
+// 2. 화살표 함수 
+const oo = () => {
+    console.log(8);
 }
 
-// 예시 사용
-let value = returnFive();  // value는 5
-console.log(value);  // 출력: 5
-
-
-
-//3. 숫자를 문자열로 변환하는 함수
-function numberToString(num) {
-    return num.toString();
+const oo1 = () => {
+    console.log("팔");
 }
 
-// 예시 사용
-let strNum = numberToString(123);  // strNum는 "123"
-console.log(strNum);  // 출력: "123"
+oo();  // 8
+oo1(); // "팔"
+
+
+
+//3. 콜백 함수
+function Five(pp) {
+    console.log(5 + pp());
+}
+
+const plus = () =>{
+    return 10;
+}
+
+Five(plus);  // = 15
+
+
+function plusNumber(값1, 값2) {
+    console.log(5 + 7 + 값1 + 값2);
+}
+
+const Pnumber = () => {
+    return 10;
+}
+
+plusNumber(Pnumber(), 3);  // 25
+
+
+
+// 4. 
+const twoNumber = (num1, num2) => {
+    if (num1 <= 3) {
+        console.log(num1 + "은 3보다 크거나 작습니다.");
+    } else {
+        console.log(num2 + "은 3보다 크네요.");
+    }
+}
+
+twoNumber(2, 10); // 2은 3보다 크거나 작습니다.
 
 
 
 
-//4. 문자열에 느낌표를 추가하는 함수
+//5. 
+const 공주머니 = ["red", "blue", "pink", "black"];
+
+// const ball = 공주머니.find(공 => 공 === "red");
+
+const ball = 공주머니.find(
+    function(공) {
+    return 공 === "red";
+});
+
+console.log("redball", ball)   // <- redball red
+
+
+
+
+//6. 문자열에 느낌표를 추가하는 함수
 function addExclamation(str) {
     return str + "!";
 }
@@ -35,33 +83,6 @@ function addExclamation(str) {
 // 예시 사용
 let excited = addExclamation("안녕하세요");  // excited는 "안녕하세요!"
 console.log(excited);  // 출력: 안녕하세요!
-
-
-
-
-//5. 숫자의 절댓값을 반환하는 함수
-function absoluteValue(num) {
-    return Math.abs(num);
-}
-
-// 예시 사용
-let absVal = absoluteValue(-7);  // absVal는 7
-console.log(absVal);  // 출력: 7
-
-
-
-
-//6. 숫자가 양수인지 확인하는 함수
-function isPositive(num) {
-    return num > 0;
-}
-
-// 예시 사용
-let isPos = isPositive(10);  // isPos는 true
-console.log(isPos);  // 출력: true
-
-isPos = isPositive(-5);  // isPos는 false
-console.log(isPos);  // 출력: false
 
 
 
@@ -97,9 +118,8 @@ function concatenate(str1, str2) {
 }
 
 // 예시 사용
-let combined = concatenate("Hello, ", "World!");  // combined는 "Hello, World!"
+const combined = concatenate("Hello, ", "World!");  // combined는 "Hello, World!"
 console.log(combined);  // 출력: Hello, World!
-
 
 
 
@@ -109,5 +129,5 @@ function getLength(str) {
 }
 
 // 예시 사용
-let length = getLength("hello");  // length는 5
+const length = getLength("hello");  // length는 5
 console.log(length);  // 출력: 5
