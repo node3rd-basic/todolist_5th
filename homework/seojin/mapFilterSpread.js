80e714fdc7b2c09192e79d1f5f22a844466d9fe9
Parkseojin2 <164994909+Parkseojin2@users.noreply.github.com>
2024-05-30 21:33:53 +0900

    Update mapFilterSpread.js

--- homework/seojin/mapFilterSpread.js
+++ homework/seojin/mapFilterSpread.js
@@ -3,4 +3,21 @@
 const colors = ["빨강", "파랑", "노랑", "초록", "보라", "주황"];
 
 
-const 반표기된_colors = colors.map((color => color === "빨강"))
+const colors_cloth = colors.map((color => color === "빨강" ? colors + "옷감" : color + "공"))
+
+console.log(colors_cloth);
+
+// filter
+
+const basic_colors = colors_cloth.filter(color => {
+    if(color.includes("옷감")) {
+        return true
+    }else{
+        return false
+    }
+})
+
+// Spread
+
+const copiedColors = [...colors];
+console.log(copiedColors);
