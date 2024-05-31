54fff8afdfdab3b15f2bbc607627c7a0d6c865e2
hyodong2 <167263781+hyodong2@users.noreply.github.com>
2024-05-31 11:40:32 +0900

    맵 필터 스프레드 연습 및 todo리스트

--- homework/donghyo/backend/app.js
+++ homework/donghyo/backend/app.js
@@ -2,9 +2,31 @@ const express = require("express");
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => {
-  res.send("안녕하십니까!");
+app.get("/todo_items", (req, res) => {
+  res.send([
+    {id:1, content: "개인과제하기"},
+    {id:2, content: "밥먹기"},
+    {id:3, content: "TIL 작성"},
+    {id:4, content: "알고리즘 코드카타"},
+    {id:5, content: "운동하기"},
+    {id:6, content: "팀프로젝트"},
+  ]);
 });
+
+app.get('/todo_items/:id', (req, res)=>{
+  const id = Number(req.params.id)
+  const todoItems = [
+    {id:1, content: "개인과제하기"},
+    {id:2, content: "밥먹기"},
+    {id:3, content: "TIL 작성"},
+    {id:4, content: "알고리즘 코드카타"},
+    {id:5, content: "운동하기"},
+    {id:6, content: "팀프로젝트"},
+  ];
+})
+
+const todoItem =todoItems.find(todoItem => todoItem.id == id)
+res.send(todoItem)
 
 app.listen(port, () => {
   console.log(`${port}로 서버가 열렸습니다!`);
