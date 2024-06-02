2ae02602289637d9813f6123297ecf8eed97064a
lys <projectys29@gmail.com>
2024-06-03 08:58:43 +0900

    cors 불러옴, 할 일 목록 조회 api 개선

--- homework/lee-yeonseo/backend/app.js
+++ homework/lee-yeonseo/backend/app.js
@@ -1,23 +1,37 @@
-const express = require('express');
+import express  from 'express';
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
+
+const todoItems = [
+  {
+    "id": 1,
+    "userId": 1,
+    "title": "할일1",
+    "doneAt": "2021-08-01",
+    "createdAt": "2021-08-01",
+    "updatedAt": "2021-08-01"
+  },
+  {
+    "id": 2,
+    "userId": 1,
+    "title": "할일2",
+    "doneAt": "2021-08-01",
+    "createdAt": "2021-08-01",
+    "updatedAt": "2021-08-01"
+  }
+]
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('안녕하세요');
 });
 
+//할일 목록 조회 api
 app.get('/todo-items', (req, res) => {
-  const todos = [
-    { id: 1, task: '코딩 연습하기' },
-    { id: 2, task: '과제하기' },
-    { id: 3, task: '책 읽기' },
-    { id: 4, task: '운동하기' },
-    { id: 5, task: '친구와 만나기' },
-    { id: 6, task: '쇼핑하기' },
-  ];
-
-  return res.status(200).json({ todos });
+  return res.send(todoItems)
 });
 
 app.get('/todo-items/:todoId', (req, res) => {
