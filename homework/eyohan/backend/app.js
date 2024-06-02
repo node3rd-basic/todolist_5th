0c5c65f4c0e9c56d683020866ba8f9c33d157f38
eyohani <144806351+eyohani@users.noreply.github.com>
2024-06-02 23:38:58 +0900

    커밋

--- homework/eyohan/backend/app.js
+++ homework/eyohan/backend/app.js
@@ -1,11 +1,58 @@
-const express = require("express");
-const app = express();
-const port = 3000
+import express from "express";
+import cors from "cors";
 
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
 app.get("/", (req, res) => {
-  res.send("안녕하세요");
+  res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log(`${port}로 서버가 열렸습니다!`);
+app.get("/movie", (req, res) => {
+  const moviedata = [
+    { id: 1, name: "영화1", 개봉일: "2024-05-11" },
+    { id: 2, name: "영화2", 개봉일: "2024-05-12" },
+    { id: 3, name: "영화3", 개봉일: "2024-05-13" },
+    { id: 4, name: "영화4", 개봉일: "2024-05-14" },
+    { id: 5, name: "영화4", 개봉일: "2024-05-14" },
+    { id: 6, name: "영화4", 개봉일: "2024-05-14" },
+  ];
+  res.send(moviedata);
+});
+
+app.get("/book", (req, res) => {
+  const bookdata = [
+    { id: 1, name: "책1", 개봉일: "2024-05-21" },
+    { id: 2, name: "책2", 개봉일: "2024-05-22" },
+    { id: 3, name: "책3", 개봉일: "2024-05-23" },
+    { id: 4, name: "책4", 개봉일: "2024-05-24" },
+  ];
+  res.send(bookdata);
+});
+
+app.get("/work", (req, res) => {
+  const workdata = [
+    { id: 1, title: "코드카타 풀기", date: "2024-06-01" },
+    { id: 1, title: "TIL 쓰기", date: "2024-06-01" },
+    { id: 1, title: "강의 복습하기", date: "2024-06-01" },
+    { id: 1, title: "연습문제 풀어보기", date: "2024-06-01" },
+  ];
+  res.send(workdata);
+});
+
+app.get("/work/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const workdata = [
+    { id: 1, title: "코드카타 풀기", date: "2024-06-01" },
+    { id: 2, title: "TIL 쓰기", date: "2024-06-01" },
+    { id: 3, title: "강의 복습하기", date: "2024-06-01" },
+    { id: 4, title: "연습문제 풀어보기", date: "2024-06-01" },
+  ];
+  const selectdata = workdata.find((el) => el.id === Number(userId));
+  res.send(selectdata);
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT}포트번호에 연결되었습니다.`);
 });
