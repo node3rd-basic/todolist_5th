74d794e9fe3080a5238080d1917ed0d0143ad382
GinGer-JSK <jskim5701@gmail.com>
2024-06-03 03:50:49 +0900

    할일목록 상세조회 기능 개발
    할일목록 상세조회를 :id 를 통해 조회 할 수 있도록 기능 개발

--- homework/jinseo_kim/backend/app.js
+++ homework/jinseo_kim/backend/app.js
@@ -1,27 +1,97 @@
+// Express 설정
 import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World?, i am express");
-});
+// CORS 설정
+import cors from "cors";
+app.use(cors());
 
+// JSON 형식의 요청 본문파싱
+app.use(express.json());
+
+// Express 실행
 app.listen(port, () => {
   console.log(`${port}로 서버가 열렸습니다!`);
 });
 
-// app.get 으로 할일목록 , 할일 목록중 한개 조회 하기 API 생성
-const todolists = [
-  { id: 1, task: "SA 작성" },
-  { id: 2, task: "API 명세서 작성 " },
-  { id: 3, task: "와이어프레임 작성" },
-  { id: 4, task: "API 할당" },
-  { id: 5, task: "기능구현" },
-  { id: 6, task: "프로젝트 테스트" },
-  { id: 7, task: "퍼블리싱" },
+// 할일목록 전역변수 선언 및 데이터 할당
+const todoItems = [
+  {
+    id: 1,
+    userId: 1,
+    title: "SA 작성",
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 2,
+    userId: 2,
+    title: "API 명세서 작성 ",
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 3,
+    userId: 3,
+    title: "와이어프레임 작성",
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 4,
+    userId: 4,
+    title: "API 할당",
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 5,
+    userId: 5,
+    title: "기능구현",
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 6,
+    userId: 6,
+    title: "프로젝트 테스트",
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 7,
+    userId: 7,
+    title: "퍼블리싱",
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
 ];
-app.get("/todolists", (req, res, next) => {
-  return res.send(todolists);
+
+// LocalHost:3000 접속시 Hello World 출력
+app.get("/", (req, res) => {
+  res.send("Hello, World?, i am express");
 });
 
-//
+// app.get 할일목록 전체조회
+app.get("/todo-items", (req, res, next) => {
+  return res.send(todoItems);
+});
+
+// app.get /todo-items/:id 할일목록 상세조회
+app.get("/todo-items/:id", (req, res) => {
+  const id = Number(req.params.id);
+  // 요청된(/todo-items/:id 로 들어온 값) 값을 Number로 변환 하고 id변수에 담는다
+  const todoItem = todoItems.find((todoItem) => todoItem.id === id);
+  // 위에서 저장된 id값과 전역변수로 지정된 todoItems배열을 순회하며
+  // 일치하는 첫번째 값을 찾고 todoItem 상수에 저장한다.
+  res.send(todoItem);
+  // todoItem 상수값을 res.send로 보낸다.
+});
