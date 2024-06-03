ea41a2673d7b977cbfd52a68f45e0e3ef0ab5949
junhowoo <junho.woo@musinsa.com>
2024-06-03 21:37:15 +0900

    6회차 강의

--- homework/woo-junho/backend/app.js
+++ homework/woo-junho/backend/app.js
@@ -9,7 +9,22 @@ app.use(cors())
 app.use(express.json())
 
 const todoItems = [
-
+    {
+        id: 1,
+        userId: 1,
+        title: "할일1",
+        doneAt: null,
+        createdAt: "2021-08-01",
+        updatedAt: "2021-08-01",
+    },
+    {
+        id: 2,
+        userId: 1,
+        title: "할일1",
+        doneAt: null,
+        createdAt: "2021-08-01",
+        updatedAt: "2021-08-01",
+    },
 ]
 
 app.get("/todo-items", (req, res) => {
@@ -34,16 +49,63 @@ app.post("/todo-items", (req, res) => {
 
 app.get("/todo-items/:id", (req, res) => {
     const id = Number(req.params.id)
-    const todoItems = [
-        {id: 1, name: "일기 쓰기"},
-        {id: 2, name: "과제 하기"},
-        {id: 3, name: "잠자기"},
-        {id: 4, name: "게임하기"},
-        {id: 5, name: "운동하기"},
-    ];
-
     const todoItem = todoItems.find(todoItem => todoItem.id === id)
     res.send(todoItem)
+})
+
+app.put("/todo-items/:id", (req, res) => {
+    const id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.status(400).send({
+            "result": false,
+            "message": "id 는 숫자여야 합니다."
+        })
+        return
+    }
+
+    const selectedTodoItem = todoItems.find(todoItem => todoItem.id === id)
+    if (!selectedTodoItem) {
+        res.status(404).send({
+            "result": false,
+            "message": "해당 아이디를 가진 todo item 이 없습니다."
+        })
+        return
+    }
+
+    const todoItemIndex = todoItems.indexOf(selectedTodoItem)
+    todoItems.splice(todoItemIndex, 1,
+        {
+            ...selectedTodoItem,
+            doneAt: selectedTodoItem.doneAt == null ? new Date() : null
+        })
+    res.send({ result: true})
+})
+
+app.delete("/todo-items/:id", (req, res) => {
+    const { id } = req.params
+    const idAsNumber = Number(id)
+    if (isNaN(idAsNumber)) {
+        res.status(400).send({
+            "result": false,
+            "message": "id 는 숫자여야 합니다."
+        })
+        return
+    }
+
+    const indexToDelete = todoItems.findIndex(todoItem => todoItem.id === idAsNumber)
+
+    if (indexToDelete === -1) {
+        res.status(404).send({
+            "result": false,
+            "message": "해당 아이디를 가진 todo item 이 없습니다."
+        })
+        return
+    }
+
+    todoItems.splice(indexToDelete, 1)
+    res.send({
+        "result": true
+    })
 })
 
 
