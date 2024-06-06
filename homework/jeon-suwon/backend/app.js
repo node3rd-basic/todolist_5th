1493f46681d273f3cace9d99a8b6b8250a075d0c
jeonsuwon <swjeon0330@gmail.com>
2024-06-06 21:24:27 +0900

    make-todolist-changeAPI

--- homework/jeon-suwon/backend/app.js
+++ homework/jeon-suwon/backend/app.js
@@ -5,53 +5,97 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/movie", (req, res) => {
-  const moviedata = [
-    { id: 1, name: "영화1", 개봉일: "2024-05-11" },
-    { id: 2, name: "영화2", 개봉일: "2024-05-12" },
-    { id: 3, name: "영화3", 개봉일: "2024-05-13" },
-    { id: 4, name: "영화4", 개봉일: "2024-05-14" },
-    { id: 5, name: "영화4", 개봉일: "2024-05-14" },
-    { id: 6, name: "영화4", 개봉일: "2024-05-14" },
-  ];
-  res.send(moviedata);
+const tododata = [
+  {
+    id: 2,
+    userId: 1,
+    title: "TIL작성하기",
+    doneAt: null,
+    createdAt: "2021-08-01",
+    updatedAt: "2021-08-01",
+  },
+  {
+    id: 5,
+    userId: 1,
+    title: "과제하기",
+    doneAt: "2021-08-01",
+    createdAt: "2021-08-01",
+    updatedAt: "2021-08-01",
+  },
+];
+
+// 조회API
+app.get("/todo-items", (req, res) => {
+  res.send(tododata);
 });
 
-app.get("/book", (req, res) => {
-  const bookdata = [
-    { id: 1, name: "책1", 개봉일: "2024-05-21" },
-    { id: 2, name: "책2", 개봉일: "2024-05-22" },
-    { id: 3, name: "책3", 개봉일: "2024-05-23" },
-    { id: 4, name: "책4", 개봉일: "2024-05-24" },
-  ];
-  res.send(bookdata);
+// 추가API
+app.post("/todo-items", (req, res) => {
+  const { title } = req.body;
+
+  const makeid = (data) => {
+    for (let i = 0; i < data.length; i++) {
+      const id = data[i].id;
+      if (!id) {
+        return 1;
+      } else if (id !== i + 1) {
+        return i + 1;
+      }
+    }
+    return id + 1;
+  };
+
+  const items = {
+    id: makeid(tododata),
+    userId: 1,
+    title: title,
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+  tododata.push(items);
+  return res.send(items);
 });
 
-app.get("/work", (req, res) => {
-  const workdata = [
-    { id: 1, title: "공부하기", date: "2024-05-21" },
-    { id: 2, title: "운동하기", date: "2024-05-21" },
-    { id: 3, title: "강의듣기", date: "2024-05-21" },
-    { id: 4, title: "잠자기", date: "2024-05-21" },
-  ];
-  res.send(workdata);
-});
-
-app.get("/work/:userId", (req, res) => {
+//특정todolist/API조회
+app.get("/todo-items/:userId", (req, res) => {
   const userId = req.params.userId;
-  const workdata = [
-    { id: 1, title: "공부하기", date: "2024-05-21" },
-    { id: 2, title: "운동하기", date: "2024-05-21" },
-    { id: 3, title: "강의듣기", date: "2024-05-21" },
-    { id: 4, title: "잠자기", date: "2024-05-21" },
-  ];
-  const selectdata = workdata.find((el) => el.id === Number(userId));
+
+  const selectdata = tododata.find((el) => el.id === Number(userId));
   res.send(selectdata);
+});
+
+//변경API
+app.post("/todo-items/:userId", (req, res) => {
+  const userId = req.params.userId;
+  console.log("userId:" + userId);
+  const { title } = req.body;
+
+  const selectData = tododata.findIndex((el) => el.id === Number(userId));
+  console.log("selectData:" + selectData);
+  if (selectData === -1) {
+    return res.json({ massege: "없는 Id입니다." });
+  }
+
+  const changeData = {
+    id: userId,
+    userId: 1,
+    title: title,
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  tododata.splice(selectData, 1, changeData);
+
+  return res.json({ changeData });
 });
 
 app.listen(PORT, () => {
