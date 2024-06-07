683c8a6cc65b8021a8b0fb232a01ad78e0733581
lys <projectys29@gmail.com>
2024-06-07 21:54:21 +0900

    로그인, 토큰 검증, 회원가입 api

--- homework/lee-yeonseo/backend/app.js
+++ homework/lee-yeonseo/backend/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 const port = 3000;
+
+const secretKey = 'aksjhdfjkladhfklhjaskl';
 
 const todoItems = [
   {
@@ -23,6 +26,16 @@ const todoItems = [
   },
 ];
 
+const users = [
+  {
+    id: 1,
+    email: 'dldustj0209@naver.com',
+    password: 'd',
+    role: 'student',
+    name: '이연서',
+  },
+];
+
 app.use(cors());
 app.use(express.json());
 
@@ -32,7 +45,13 @@ app.get('/', (req, res) => {
 
 //할 일 목록 조회 api
 app.get('/todo-items', (req, res) => {
-  return res.send(todoItems);
+  const token = req.headers.authorization;
+  try {
+    const user = jwt.verify(token, secretKey);
+    return res.send(todoItems.filter((todoItem) => todoItem.userId === user.id));
+  } catch (error) {
+    return res.status(401).json({ message: '인증정보가 유효하지 않습니다.' });
+  }
 });
 
 //할 일 상세 조회 api
@@ -46,21 +65,26 @@ app.get('/todo-items/:id', (req, res) => {
 //할 일 등록 api
 app.post('/todo-items', (req, res) => {
   const { title } = req.body;
+  const token = req.headers.authorization;
 
-  const newTodoId = todoItems[todoItems.length - 1] ? todoItems[todoItems.length - 1].id + 1 : 1;
+  try {
+    const user = jwt.verify(token, secretKey);
+    const newTodoId = todoItems[todoItems.length - 1] ? todoItems[todoItems.length - 1].id + 1 : 1;
 
-  const newTodoItem = {
-    id: newTodoId,
-    userId: 1,
-    title,
-    doneAt: null,
-    createdAt: new Date(),
-    updatedAt: null,
-  };
+    const newTodoItem = {
+      id: newTodoId,
+      userId: user.id,
+      title,
+      doneAt: null,
+      createdAt: new Date(),
+      updatedAt: null,
+    };
 
-  todoItems.push(newTodoItem);
-
-  return res.send(newTodoItem);
+    todoItems.push(newTodoItem);
+    return res.send(newTodoItem);
+  } catch (error) {
+    return res.status(401).json({ message: '인증정보가 유효하지 않습니다.' });
+  }
 });
 
 //할 일 수정 api
@@ -106,6 +130,49 @@ app.delete('/todo-items/:id', (req, res) => {
   todoItems.splice(todoItemIndex, 1);
 
   return res.send(todoItems);
+});
+
+//회원가입 api
+app.post('/sign-up', (req, res) => {
+  const { email, password, rePassword, role, name } = req.body;
+  if (!email || !password || !rePassword || !role || !name) {
+    return res.status(400).json({ message: '필수 입력값이 누락되었습니다.' });
+  }
+  if (password !== rePassword) {
+    return res.status(400).json({ message: '비밀번호와 비밀번호 확인이 일치하지 않습니다.' });
+  }
+  const existingUser = users.find((user) => user.email === email);
+  if (existingUser) {
+    return res.status(409).json({ message: '이미 가입한 이메일입니다.' });
+  }
+  const id = users.length === 0 ? 1 : users[users.length - 1].id + 1;
+  const newUser = { id, email, password, role, name };
+
+  users.push(newUser);
+  return res.status(200).json({ newUser });
+});
+
+//로그인 api
+app.post('/sign-in', (req, res) => {
+  const { email, password } = req.body;
+  const { password: _pw, ...user } = users.find((user) => user.email === email && user.password === password);
+  if (!user) {
+    return res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+  }
+  console.log(user);
+  const token = jwt.sign(user, secretKey);
+  return res.status(200).json({ token });
+});
+
+//토큰 검증 api
+app.get('/users/me', (req, res) => {
+  const token = req.headers.authorization;
+  try {
+    const user = jwt.verify(token, secretKey);
+    return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(401).json({ message: '인증정보가 유효하지 않습니다.' });
+  }
 });
 
 app.listen(port, () => {
