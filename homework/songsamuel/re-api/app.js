bb2c157bf4db4324ada9d5b477947ec0e560ece0
songsamuel <songsme2886@gmail.com>
2024-06-14 22:25:03 +0900

    목록들 조회, 목록 한 개 조회, 할 일 등록, 완료 여부 토글 작성 완료 및 테스트 완료

--- homework/songsamuel/re-api/app.js
+++ homework/songsamuel/re-api/app.js
@@ -22,7 +22,7 @@ const users = [
   },
 ];
 
-const TodoItem = [
+const todoItem = [
   {
     id: 1,
     userId: 1,
@@ -43,22 +43,76 @@ const TodoItem = [
 
 // 목록들 조회
 app.get("/todo-items", (req, res) => {
-  return res.send("목록들 조회가 완료되었습니다.");
+  // 목록을 가져오는데 무슨 목록을 가져오는것인가? 내가 작성한 것만 가져온다.
+  // 그럼 로그인 한 것들 가져올 때 같이 가져온다.
+  const token = req.headers.authorization;
+
+  const user = jwt.verify(token, secretKey);
+
+  // todoItem에서 내가 작성한 것들만 가져올 것인데 그 구분을 뭐로 하냐? userId로 한다
+
+  // 이거 못 쓰는 이유 api 스펙이서 나온대로 가 안된다. mytodoItem = {} 이런 모습이 나오기 때문에 쓰면 안된다.
+  //   const mytodoItem = todoItem.filter((item) => item.userId === user.id);
+
+  res.status(200).json(todoItem.filter((item) => item.userId === user.id));
+  return;
 });
 
 // 할 일 목록 한개 조회
 app.get("/todo-items/:id", (req, res) => {
-  return res.send("목록들 중 한개가 완료되었습니다.");
+  // 한개를 조회하려면 뭘 써야해? 내가 입력 받은 값을 써야해 그거 뭐다? params
+  const { id } = req.params;
+  const todoId = Number(id);
+  // 할 일 목록 한개를 조회하려면 내 정보를 가져와야한다. = 토큰
+  const token = req.headers.authorization;
+
+  // 가져온 토큰을 통해서 정보를 꺼내야한다. very
+
+  const user = jwt.verify(token, secretKey);
+
+  //   // 이거 이대로 넣지 마라잉?!
+  //   const oneTodoItem = todoItem.find((item) => item.id === todoId);
+
+  res.status(200).json(todoItem.find((item) => item.id === todoId));
+  return;
 });
 
-// 키워드를 통한 할 일 목록들 조회
+// 키워드를 통한 할 일 목록들 조회  // 지금은 안하는 것
 app.get("/todo-items/search/:keyword", (req, res) => {
   return res.send("키워드를 통한 목록 조회가 완료되었습니다.");
 });
 
 // 할 일 등록
 app.post("/todo-items", (req, res) => {
-  return res.json({ msg: "할일 등록이 완료되었습니다." });
+  // 1. 할 일을 받을 값을 req로 받는다.
+  const { title } = req.body;
+  const token = req.headers.authorization;
+  // 2. 작성 할 수 있도록 인증을 받은 토큰을 가져온다.
+  const user = jwt.verify(token, secretKey);
+
+  const newId = todoItem[todoItem.length - 1]
+    ? todoItem[todoItem.length - 1].id + 1
+    : 1;
+
+  const newItem = {
+    id: newId,
+    userId: user.id,
+    title: title,
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  };
+
+  todoItem.push(newItem);
+
+  res.status(201).json(newItem);
+  return;
+});
+
+// 할일 완료 여부 토글
+
+app.put("/todo-items/:id", (req, res) => {
+  res.status(200).json({ result: true });
 });
 
 // 할일 삭제
@@ -143,6 +197,7 @@ app.post("/sign-up", (req, res) => {
 app.get("/users/me", (req, res) => {
   const token = req.headers.authorization;
 
+  //내가 작성한 코드
   // 가져온 토큰을 검증해서 거기서 내 정보를 꺼내야한다.
   const user = jwt.verify(token, secretKey);
   // 만약 잘못된 시크릿 키를 가져왔으면
@@ -150,7 +205,8 @@ app.get("/users/me", (req, res) => {
     res.status(400).json({ message: "당신은 권한이 없어!" });
   }
 
-  res.status(200).json({ user });
+  // 내가 작성한 코드 json({user});로 하면 undefind가 나온다. 왜냐? api 스펙을 보면 user를 풀어 헤친 모습 {}를 하면 감싼 모습!
+  res.status(200).json(user);
   return;
 });
 
