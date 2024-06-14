a5dd76eabbf9a8d2e7b47189d48a05ec29bd0c12
GinGer-JSK <jskim5701@gmail.com>
2024-06-14 20:26:04 +0900

    회원가입 API구현

--- homework/jinseo_kim/re-api/app.js
+++ homework/jinseo_kim/re-api/app.js
@@ -4,15 +4,47 @@ import cors from "cors";
 const port = 3000;
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`서버오픈, ${port} 포트`);
 });
 
 // api spec을 보고 api controller 부터 작성한다.
 
+const users = [];
 // 회원가입 API
 app.post("/sign-up", (req, res) => {
+  // req.body 를 통해 api spec을 받아서 저장한다.
+  const { email, password, rePassword, role, name } = req.body;
+  // 입력받은 값에 빈칸이 없는지 검증한다. 필수값이 누락된 경우 400에러 처리
+  if (!email || !password || !rePassword || !role || !name) {
+    res.status(400).send({ message: "필수값이 누락되었습니다." });
+    return;
+  }
+  // 이메일 중복 없는지 검증한다, 중복 이메일 가입시 409 에러처리
+  const extUser = users.find((usr) => usr.email === email);
+  if (extUser) {
+    res.status(409).send({ messagse: "중복이다 이녀석아" });
+    return;
+  }
+  // 비밀번호와 비밀번호 확인이 일치하는지 확인, 일치하지 않을경우 400 에러처리
+  if (password !== rePassword) {
+    res.status(400).send({ messagse: "비번을 확인하거라" });
+    return;
+  }
+  // id 값을 검증하여 +1, 1 을 준다.
+  //users의 길이가 0일때 > id에 1을 넣는다.
+  //users의 길이가 0이 아닐때 > id에 users.length의 길이 -1 하고 +1을 넣는다.
+  const id = users.length === 0 ? 1 : users[users.length - 1].id + 1;
+  // 모든로직 통과하면 newUser에 저장하고
+  const newUser = { email, password, role, name };
+  // users에 push 한다.
+  users.push(newUser);
+  // json 으로 newUser를 보여준다.
   res.send({ message: "회원가입이다." });
+  console.log(newUser);
 });
 
 // 로그인 API
