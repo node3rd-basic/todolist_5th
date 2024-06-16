f9dba703c249cd84f79365306641a3de8b7659d3
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-06-16 11:20:45 +0900

    crud 다시 만들어보기

--- homework/najiyoon/re-api/app.js
+++ homework/najiyoon/re-api/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import jwt from "jsonwebtoken";
 
@@ -20,6 +20,62 @@ const users = [
 ];
 
 const secretKey = "sdhtfgwgrljkw";
+
+const TodoItems = [
+  {
+    id: 1,
+    userId: 1,
+    title: "할일1",
+    doneAt: "2021-08-01",
+    createdAt: "2021-08-01",
+    updatedAt: "2021-08-01",
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: "할일2",
+    doneAt: "2021-08-01",
+    createdAt: "2021-08-01",
+    updatedAt: "2021-08-01",
+  },
+];
+
+// 다시 확인 할 것 ; 배열이므로 ; 유저아이디 처럼
+const getIncrementedId = (arr) => {
+  return arr[TodoItems.length - 1] ? arr[TodoItems.length - 1].id + 1 : 1;
+};
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
+  try {
+    req.user = jwt.verify(token, secretKey);
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "인증정보가 틀렸습니다." });
+  }
+};
+
+//아이디 하나 조회
+const validateTodoitemId = (req) => {
+  const idAsNumber = Number(req.params.id);
+  if (isNaN(idAsNumber)) {
+    throw new Error("id가 숫자가 아닙니다.");
+  }
+  return idAsNumber;
+};
+//!!!!주석처리!!!!!
+
+const getTodoItemsById = (id) => {
+  console.log(getTodoItemsById);
+  console.log(id);
+  const todoItem = TodoItems.find((todoItem) => todoItem.id === id);
+  console.log(todoItem);
+  if (!todoItem) {
+    throw new Error("찾을수 없음");
+  }
+  console.log("todoItem-->", todoItem);
+  return todoItem;
+};
 
 //회원가입, 로그인, 인증 : 리턴확인
 //회원가입 :
@@ -96,19 +152,67 @@ app.get("/users/me", (req, res) => {
 });
 
 //할일목록들 조회 : 응답[] id, userid, title, doneAt, createdAt, updatedAt
-app.get("/todo-items", (req, res) => {});
+app.get("/todo-items", authMiddleware, (req, res) => {
+  console.log(req.user);
+  const user = req.user;
+  res.send(TodoItems.filter((TodoItems) => TodoItems.userId === user.id));
+});
 //할일목록 한개 조회 id, userid, title, doneAt, createdAt, updatedAt
-app.get("/todo-items/;id", (req, res) => {});
+app.get("/todo-items/;id", authMiddleware, (req, res) => {
+  const id = validateTodoitemId(req);
+  const TodoItems = getTodoItemsById(id);
+  res.send(TodoItems);
+});
 
-//할일목록들 조회 : 키워드로
-app.get("/todo-items/search/:keyword", (req, res) => {});
-//할일등록 req: title
-app.post("/todo-itmes", (req, res) => {});
+//할일목록들 조회 : 키워드로 : 필터 / 완료
+// app.get("/todo-items/search/:keyword", authMiddleware, (req, res) => {
+//     const user = req.user;
+//     TodoItems.filter(키워드)
+// });
+//할일등록 req: title :미들웨어
+app.post("/todo-items", authMiddleware, (req, res) => {
+  const user = req.user;
+  const { title } = req.body;
+  const newId = getIncrementedId(TodoItems);
+  const newTodoItem = {
+    id: newId,
+    userId: user.id,
+    title: title,
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: "2021-08-01",
+  };
+
+  TodoItems.push(newTodoItem);
+  res.send(newTodoItem);
+});
 
 //할일 수정 : res. result
-app.put("/todo-itmes/:id", (req, res) => {});
+app.put("/todo-items/:id", authMiddleware, (req, res) => {
+  const id = validateTodoitemId(req);
+  const selectedTodoItem = getTodoItemsById(id);
+  const todoItemIndex = TodoItems.indexOf(selectedTodoItem);
+  TodoItems.splice(todoItemIndex, 1, {
+    ...selectedTodoItem,
+    doneAt: selectedTodoItem.doneAt == null ? new Date() : null,
+  });
+  res.send({ result: true });
+});
 
 //할일 삭제
-app.delete("/todo-itmes/:id", (req, res) => {});
+app.delete("/todo-items/:id", authMiddleware, (req, res) => {
+  console.log(req.params);
+  const id = validateTodoitemId(req);
+  const selectedTodoItem = getTodoItemsById(id);
+  const indexToDelete = TodoItems.indexOf(selectedTodoItem);
+
+  TodoItems.splice(indexToDelete, 1);
+  res.send({ result: true });
+});
+
+const errorMiddleware = (err, req, res, next) => {
+  //err message
+  res.status(500).json({ message: "에러입니다." });
+};
 
 app.listen(PORT, () => console.log(PORT, "서버열림"));
