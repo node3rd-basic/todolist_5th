e8e6600e0dc60548f7055e018e6a1226a042fbd9
lys <projectys29@gmail.com>
2024-06-18 17:19:42 +0900

    refactor: auth미들웨어 분리 userController 분리

--- homework/lee-yeonseo/backend/app.js
+++ homework/lee-yeonseo/backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import jwt from 'jsonwebtoken';
+import authMiddleware from '../backend/middlewares/auth.middleware.js';
+import * as userController from './controllers/user.controller.js';
 
 const app = express();
 const port = 3000;
@@ -11,23 +12,8 @@ const errorHandlingMiddleware = (err, req, res, next) => {
   });
 };
 
-const secretKey = 'aksjhdfjkladhfklhjaskl';
-
 app.use(cors());
 app.use(express.json());
-
-//사용자 인증 미들웨어
-const authMiddleware = (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
-    const user = jwt.verify(token, secretKey);
-
-    req.user = user;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: '권한이 없습니다.' });
-  }
-};
 
 //입력 투두 아이디 검증
 const todoItemIdValidator = (req, res, next) => {
@@ -47,9 +33,6 @@ const getTodoItemById = (id) => {
   }
   return todoItem;
 };
-
-//투두 아이템 , 유저 아이디 생성 //프리티어 때문에 자동 정렬....
-const getIncrementedId = (arr) => (arr[arr.length - 1] ? arr[arr.length - 1].id + 1 : 1);
 
 //투두 아이템 필터링
 const filteredTodoItems = (userId) => {
@@ -142,41 +125,13 @@ app.delete('/todo-items/:id', authMiddleware, todoItemIdValidator, (req, res) =>
 });
 
 //회원가입 api
-app.post('/sign-up', (req, res) => {
-  const { email, password, rePassword, role, name } = req.body;
-  if (!email || !password || !rePassword || !role || !name) {
-    return res.status(400).json({ message: '필수 입력값이 누락되었습니다.' });
-  }
-  if (password !== rePassword) {
-    return res.status(400).json({ message: '비밀번호와 비밀번호 확인이 일치하지 않습니다.' });
-  }
-  const existingUser = users.find((user) => user.email === email);
-  if (existingUser) {
-    return res.status(409).json({ message: '이미 가입한 이메일입니다.' });
-  }
-  const id = getIncrementedId(users);
-  const newUser = { id, email, password, role, name };
-
-  users.push(newUser);
-  return res.status(200).json({ newUser });
-});
+app.post('/sign-up', userController.postSignUp);
 
 //로그인 api
-app.post('/sign-in', (req, res) => {
-  const { email, password } = req.body;
-  const { password: _pw, ...user } = users.find((user) => user.email === email && user.password === password);
-  if (!user) {
-    return res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
-  }
-
-  const token = jwt.sign(user, secretKey);
-  return res.status(200).json({ token });
-});
+app.post('/sign-in', userController.postSignIn);
 
 //토큰 검증 api
-app.get('/users/me', authMiddleware, (req, res) => {
-  return res.json(req.user);
-});
+app.get('/users/me', authMiddleware, userController.getUserMe);
 
 app.use(errorHandlingMiddleware);
 
