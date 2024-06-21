6de9749e3d66bfe49c189fe9626cfd560582f76b
leegilhyeon <164996803+leegilhyeon@users.noreply.github.com>
2024-06-21 18:47:57 +0900

    feat: todoItem 레이어 분리

---
+++ homework/leegilhyeon/backend/controllers/todoItem.controller.js
@@ -0,0 +1,73 @@
+import todoItems from "../db/todoItems.js";
+
+const todoItemById = (id) => {
+  const todoItem = todoItems.find((todoItem) => todoItem.id === id);
+  if (!todoItem) {
+    throw new Error("해당 아이디가 가진 todoItem이 없습니다.");
+  }
+  return todoItem;
+};
+const validateTodoId = (req) => {
+  const todoId = Number(req.params.id);
+  if (isNaN(todoId)) {
+    throw new Error("Id는 숫자여야 합니다.");
+  }
+  return todoId;
+};
+
+//내 할일 목록
+export function getTodoItems(req, res) {
+  const user = req.user;
+
+  res.send(todoItems.filter((todoItem) => todoItem.userId === user.id));
+}
+
+// 할일 조회
+export function getTodoItem(req, res) {
+  try {
+    const id = validateTodoId(req);
+    const todoItem = todoItemById(id);
+    res.send(todoItem);
+  } catch (error) {
+    res.status(400).send(e.message);
+  }
+}
+
+// 할일 생성
+export function postTodoItem(req, res) {
+  const { title } = req.body;
+  const user = req.user;
+
+  const newId = incrementedTodoId(todoItems);
+  const newTodoItem = {
+    id: newId,
+    userId: user.id,
+    title: title,
+    doneAt: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  };
+  todoItems.push(newTodoItem);
+  res.send(newTodoItem);
+}
+
+// 할일 수정
+export function putTodoItem(req, res) {
+  const id = validateIdMiddleware(req);
+  //해당 id를 가지고 있는 todoitem 찾기
+  const todoItemFind = todoItemById(id);
+  const todoItemIndex = todoItems.indexOf(todoItemFind);
+  todoItems.splice(todoItemIndex, 1, {
+    ...todoItemFind,
+    doneAt: todoItemFind.doneAt == null ? new Date() : null,
+  });
+  res.send({ message: "수정되었습니다." });
+}
+
+// 할일 삭제
+export function deleteTodoItem(req, res) {
+  const id = validateIdMiddleware(req);
+  const deleteTodoItem = todoItemById(id);
+  todoItems.splice(deleteTodoItem, 1);
+  res.send({ message: "todoItem이 삭제되었습니다." });
+}
---
+++ homework/leegilhyeon/backend/controllers/user.controller.js
@@ -0,0 +1,6 @@
+import users from "../db/users.js";
+
+const secretKey = "wewqsfaserafgf";
+
+const incrementedTodoId = (arr) =>
+  arr[todoItems.length - 1] ? arr[todoItems.length - 1].id + 1 : 1;
