51c9a7affe505909f1bcebaa5b9371e4bd67e49b
lys <projectys29@gmail.com>
2024-06-21 22:28:43 +0900

    refactor: 유저 api 레이어 분리(서비스, 저장소)

--- homework/lee-yeonseo/backend/controllers/user.controller.js
+++ homework/lee-yeonseo/backend/controllers/user.controller.js
@@ -1,11 +1,6 @@
-import users from '../db/users.js';
-import jwt from 'jsonwebtoken';
+import * as userService from '../services/user.service.js';
 
-const tokenSecretKey = 'aksjhdfjkladhfklhjaskl';
-
-//투두아이템 아이디, 유저 아이디 생성
-const getIncrementedId = (arr) => (arr[arr.length - 1] ? arr[arr.length - 1].id + 1 : 1);
-
+//회원가입 (1)
 export const postSignUp = (req, res) => {
   //req.body에서 email, password, rePassword, role, name 받아오기
   const { email, password, rePassword, role, name } = req.body;
@@ -22,32 +17,12 @@ export const postSignUp = (req, res) => {
     return;
   }
 
-  //이메일 중복 확인
-  const existedEmail = users.find((user) => user.email === email);
-  if (existedEmail) {
-    res.status(400).json({ message: '이미 가입된 이메일입니다.' });
-    return;
-  }
-
-  //유저 아이디 만들어주기
-  const newUserId = getIncrementedId(users);
-
-  //newUser 생성
-  const newUser = {
-    id: newUserId,
-    email,
-    password,
-    name,
-    role,
-    createdAt: new Date(),
-  };
-
-  //users배열에 newUser 넣어주기
-  users.push(newUser);
+  const newUser = userService.signUp(email, password, role, name);
 
   res.status(201).json(newUser);
 };
 
+//로그인 (2)
 export const postSignIn = (req, res) => {
   //req.body에서 email, password 받아오기
   const { email, password } = req.body;
@@ -57,20 +32,12 @@ export const postSignIn = (req, res) => {
     return;
   }
 
-  //유저 배열에 해당하는 이메일과 패스워드와 일치하는 유저가 있는지 검색
-  const { password: _pw, ...user } = users.find((user) => user.email === email && user.password === password);
-  //일치하는 유저가 없다면 오류 반환
-  if (!user) {
-    res.status(401).json({ message: '이메일 혹은 패스워드를 확인해주세요.' });
-    return;
-  }
-
-  //일치하는 유저가 있다면 패스워드를 제외한 유저 정보를 페이로드로 토큰 발급
-  const token = jwt.sign(user, tokenSecretKey);
+  const token = userService.signIn(email, password);
 
   res.status(200).json({ token });
 };
 
+//내정보 가져오기 (3)
 export const getUserMe = (req, res) => {
   //사용자 인증 미들웨어가 보내준 req.user를 반환
   res.status(200).json(req.user);
---
+++ homework/lee-yeonseo/backend/services/user.service.js
@@ -0,0 +1,48 @@
+import * as userRepository from '../repositories/user.repository.js';
+import jwt from 'jsonwebtoken';
+
+const tokenSecretKey = 'aksjhdfjkladhfklhjaskl';
+
+//회원가입
+export const signUp = (email, password, role, name) => {
+  //이메일 중복 확인
+  const existedEmail = userRepository.findUserByEmail(email);
+
+  if (existedEmail) {
+    throw new Error('이미 가입된 이메일입니다.');
+  }
+
+  //유저 아이디 만들어주기
+  const newUserId = userRepository.getIncrementedId();
+
+  //newUser 생성
+  const newUser = {
+    id: newUserId,
+    email,
+    password,
+    name,
+    role,
+    createdAt: new Date(),
+  };
+
+  //users배열에 newUser 넣어주기
+  userRepository.createUser(newUser);
+
+  return newUser;
+};
+
+//로그인
+export const signIn = (email, password) => {
+  //유저 배열에 해당하는 이메일과 패스워드와 일치하는 유저가 있는지 검색
+  const { password: _pw, ...user } = userRepository.findUser(email, password);
+
+  //일치하는 유저가 없다면 오류 반환
+  if (!user) {
+    throw new Error('유저 정보가 없습니다.');
+  }
+
+  //일치하는 유저가 있다면 패스워드를 제외한 유저 정보를 페이로드로 토큰 발급
+  const token = jwt.sign(user, tokenSecretKey);
+
+  return token;
+};
