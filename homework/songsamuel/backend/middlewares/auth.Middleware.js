b888d774d9181bee841aa26e1f4a9c3047a41fd3
songsamuel <songsme2886@gmail.com>
2024-06-24 02:19:04 +0900

    3LA 폴더 생성 및 리펙토링

---
+++ homework/songsamuel/backend/middlewares/auth.Middleware.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const secretKey = "돈 많이 벌고 싶다.";
+
+export default (req, res, next) => {
+  const token = req.headers.authorization;
+  try {
+    req.user = jwt.verify(token, secretKey);
+
+    next();
+  } catch (error) {
+    res.status(401).send({ message: "권한이 없습니다." });
+  }
+};
