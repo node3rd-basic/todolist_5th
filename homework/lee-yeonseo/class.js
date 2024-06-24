f423b437fa3c52df5b1fcf763fab4484ea3915e2
lys <projectys29@gmail.com>
2024-06-24 23:52:45 +0900

    chore: 주석 수정

--- homework/lee-yeonseo/class.js
+++ homework/lee-yeonseo/class.js
@@ -23,15 +23,15 @@ class Marguerite extends Daisy {
 }
 
 const daisy1 = new Daisy('데이지', 3);
-const marguerite1 = new Marguerite('마가렛', 4); //왜 year가 undefined?
+const marguerite1 = new Marguerite('마가렛', 4);
 
 daisy1.watering();
 
 console.log(daisy1);
 console.log(marguerite1);
 console.log('데이지1은 데이지의 인스턴스', daisy1 instanceof Daisy);
-console.log('데이지1은 플라워의 인스턴스', daisy1 instanceof Flower);
-console.log('데이지1은 마가렛의 인스턴스', daisy1 instanceof Marguerite);
+console.log('데이지1은 플라워의 인스턴스', daisy1 instanceof Flower); //true
+console.log('데이지1은 마가렛의 인스턴스', daisy1 instanceof Marguerite); //false
 console.log('-------------------');
 console.log('마가렛은 데이지의 인스턴스', marguerite1 instanceof Daisy);
 console.log('마가렛은 플라워의 인스턴스', marguerite1 instanceof Flower);
