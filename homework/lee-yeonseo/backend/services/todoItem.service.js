5e225f9a21984c11aa8b17fb44414acae373023a
lys <projectys29@gmail.com>
2024-06-24 22:40:55 +0900

    add: 커스텀 에러 만들고 연결

--- homework/lee-yeonseo/backend/services/todoItem.service.js
+++ homework/lee-yeonseo/backend/services/todoItem.service.js
@@ -1,4 +1,5 @@
 import * as todoItemRepository from '../repositories/todoItem.repository.js';
+import CustomError from '../common/custom.error.js';
 
 //해당하는 투두 아이템 아이디의 할일 찾기 // 할일 목록 상세 조회
 const findTodoItem = ({ todoItemId, userId }) => {
@@ -6,12 +7,12 @@ const findTodoItem = ({ todoItemId, userId }) => {
 
   //해당 아이디의 할일이 존재하지 않으면 오류 반환
   if (!selectedTodoItem) {
-    throw new Error('해당 아이디의 할 일이 존재하지 않습니다.');
+    throw new CustomError(404, '해당 아이디의 할 일이 존재하지 않습니다.');
   }
 
   //찾은 할일의 유저 아이디와 req.user로 받은 유저 아이디가 불일치하면 오류 반환
   if (selectedTodoItem.userId !== userId) {
-    throw new Error('접근 권한이 없는 할 일입니다.');
+    throw new CustomError(403, '접근 권한이 없는 할 일입니다.');
   }
 
   return selectedTodoItem;
