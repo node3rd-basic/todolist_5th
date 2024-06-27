5b316ef132df6c8ebd86399645e412c999f5e193
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-06-27 20:52:13 +0900

    오류수정

--- homework/najiyoon/backend/env/module.js
+++
@@ -1,2 +0,0 @@
-export default "module.js - 파일";
-console.log(process.env.JWT_SECRET_KEY, "모듈에서 로드");
--- homework/najiyoon/backend/service/users.service.js
+++ homework/najiyoon/backend/service/users.service.js
@@ -1,10 +1,6 @@
 import "dotenv/config";
-import users from "../db/users.js";
-import {
-  findUserByEmail,
-  postNewUser,
-  postUser,
-} from "../repository/users.repository.js";
+// import users from "../db/users.js";
+import * as usersRepository from "../repository/users.repository.js";
 import jwt from "jsonwebtoken";
 
 //헷갈리니 매개변수로 뭘할건지 함수명으로 표현할것
@@ -25,7 +21,7 @@ export function getNewUser(email, role, name, password, rePassword) {
   }
   //이메일 중복확인 유저의 이메일을 찾는다. 유저정보에서 찾은 정보의 이메일과 받은 이메일이 같은지
   //레파지토리에 다녀오는 통로 : 이메일 매개변수로 넘겨주고
-  const alreadyEmail = findUserByEmail(email);
+  const alreadyEmail = usersRepository.findUserByEmail(email);
 
   if (alreadyEmail) {
     res.status(409).json({
@@ -33,18 +29,21 @@ export function getNewUser(email, role, name, password, rePassword) {
     });
     return;
   }
-  //회원가입진행 : 아이디만들기 / user 만들기
-  const id = users.length === 0 ? 1 : users[users.length - 1].id + 1;
+  //회원가입진행 : 아이디만들기 / user 만들기 레파지토리로 가야하나
+  //
+  const id = usersRepository.checkAlreadyId();
+
+  // users.length === 0 ? 1 : users[users.length - 1].id + 1;
   //repository에서 넘겨받은 newUser는 postNewUser(id...에 담겨왔고,)
   //그걸 newUser에 넘김
-  const newUser = postNewUser(id, email, role, name, password);
+  const newUser = usersRepository.postNewUser(id, email, role, name, password);
   return newUser;
 }
 
 export function signIn(email, password) {
   //email, password 맞는지 확인후 토큰발급?
 
-  const userInfo = postUser(email, password);
+  const userInfo = usersRepository.postUser(email, password);
   if (!userInfo) {
     res.status(400).json({ message: "틀림" });
     return;
