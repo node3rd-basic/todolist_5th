f05c1a99004b38e5c4c36baeed97122a1bc06423
leegilhyeon <164996803+leegilhyeon@users.noreply.github.com>
2024-06-28 16:23:50 +0900

    불필요한 부분 삭제와 빠트린 부분 수정

--- homework/leegilhyeon/backend/services/todoItem.service.js
+++ homework/leegilhyeon/backend/services/todoItem.service.js
@@ -1,9 +1,10 @@
 import * as todoItemRepository from "../repository/todoItem.repository.js";
+import CustomError from "../common/custom.error.js";
 
 export function findTodoItemById(id) {
   const todoItem = todoItemRepository.findTodoItemById(id);
   if (!todoItem) {
-    throw new Error("해당 아이디가 가진 todoItem이 없습니다.");
+    throw new CustomError("해당 아이디가 가진 todoItem이 없습니다.", 404);
   }
   return todoItem;
 }
