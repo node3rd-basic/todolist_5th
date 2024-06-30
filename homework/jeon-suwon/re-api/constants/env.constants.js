1d90c4dfee07adaf1ca7a3cbb42428a827750ce3
jeonsuwon <swjeon0330@gmail.com>
2024-06-30 20:43:15 +0900

    add: .env

--- homework/jeon-suwon/re-api/constants/env.constants.js
+++ homework/jeon-suwon/re-api/constants/env.constants.js
@@ -2,4 +2,8 @@ import "dotenv/config";
 
 export const ENV_KEY = {
   SECRET_KEY: process.env.SECRET_KEY,
+  HOST: process.env.HOST,
+  PORT: process.env.PORT,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
 };
