bc031100c3dbe413c6f62e75e2f91b197a904354
jeonsuwon <swjeon0330@gmail.com>
2024-07-01 11:57:44 +0900

    refactory: mysql-signUP,signin

--- homework/jeon-suwon/re-api/common/db.js
+++ homework/jeon-suwon/re-api/common/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise"; // 기본 기능을 사용하려면 mysql2을 가져온다.
 import { ENV_KEY } from "../constants/env.constants.js";
 
-export const connection = await mysql.createConnection({
+export const conn = await mysql.createConnection({
   host: ENV_KEY.HOST,
   user: ENV_KEY.USER,
   password: ENV_KEY.PASSWORD,
