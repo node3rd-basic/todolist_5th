c771aff7002e7c45f952858c8e4dfe6f97d638fc
lys <projectys29@gmail.com>
2024-07-03 20:37:12 +0900

    fix: todoItem 관련 인젝션 방지

--- homework/lee-yeonseo/backend/repositories/todoItem.repository.js
+++ homework/lee-yeonseo/backend/repositories/todoItem.repository.js
@@ -2,38 +2,36 @@ import conn from '../common/conn.js';
 
 //할일 찾기
 export const findTodoItemById = async (todoItemId) => {
-  const [selectedTodoItem] = await conn.execute(`SELECT * FROM todo_items WHERE id = ${todoItemId}`);
+  const sql = `SELECT * FROM todo_items WHERE id = ?`;
+  const [selectedTodoItem] = await conn.execute(sql, [todoItemId]);
 
-  return selectedTodoItem[0];
+  return selectedTodoItem[0] ?? null;
 };
 
 //할일 목록 조회
 export const findTodoItemByUserId = async (userId) => {
-  const [myTodoItems] = await conn.execute(`SELECT * FROM todo_items WHERE user_id = ${userId}`);
+  const sql = `SELECT * FROM todo_items WHERE user_id = ?`;
+  const [myTodoItems] = await conn.execute(sql, [userId]);
 
-  return myTodoItems.map((myTodoItem) => {
-    return {
-      ...myTodoItem,
-      doneAt: myTodoItem.done_at,
-    };
-  });
+  return myTodoItems;
 };
 
 //할일 등록
 export const postTodoItem = async (userId, title) => {
-  const [newTodoItem] = await conn.execute(`INSERT INTO todo_items (user_id, title) VALUES ('${userId}','${title}')`);
+  const sql = `INSERT INTO todo_items (user_id, title) VALUES (:userId, :title)`;
+  const [newTodoItem] = await conn.execute(sql, { userId, title });
 
-  return { id: newTodoItem.insertId, userId, title, doneAt: null };
+  return newTodoItem.insertId;
 };
 
 //할일 완료 여부 토글
 export const todoItemDoneAt = async (todoItemId) => {
-  await conn.execute(
-    `UPDATE todo_items SET done_at = CASE WHEN done_at IS NULL THEN current_timestamp  ELSE NULL END WHERE id = ${todoItemId}`,
-  );
+  const sql = `UPDATE todo_items SET done_at = CASE WHEN done_at IS NULL THEN current_timestamp  ELSE NULL END WHERE id = ?`;
+  await conn.execute(sql, [todoItemId]);
 };
 
 //할일 삭제
 export const deleteTodoItem = async (todoItemId) => {
-  await conn.execute(`DELETE FROM todo_items WHERE id = ${todoItemId}`);
+  const sql = `DELETE FROM todo_items WHERE id = ?`;
+  await conn.execute(sql, [todoItemId]);
 };
