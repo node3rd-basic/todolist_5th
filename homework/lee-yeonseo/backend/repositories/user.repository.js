785b4224b97fe02e3cafe8e536528cd367da66ee
lys <projectys29@gmail.com>
2024-07-03 20:16:09 +0900

    fix: user 관련 인젝션 방지

--- homework/lee-yeonseo/backend/repositories/user.repository.js
+++ homework/lee-yeonseo/backend/repositories/user.repository.js
@@ -1,14 +1,16 @@
 import conn from '../common/conn.js';
 
 export const findUserByEmail = async (email) => {
-  const [users] = await conn.execute(`SELECT * FROM users WHERE email = '${email}'`);
+  const sql = `SELECT * FROM users WHERE email = ?`;
+  const [users] = await conn.execute(sql, [email]);
 
   return users[0];
 };
 
 export const createUser = async (userData) => {
-  const sql = `INSERT INTO users (email, password, name, role) VALUES ('${userData.email}', '${userData.password}', '${userData.name}', '${userData.role}')`;
-  const result = await conn.execute(sql);
+  const sql = `INSERT INTO users (email, password, role, name) VALUES (:email, :password, :role, :name)`;
+  const result = await conn.execute(sql, userData);
+
   return {
     id: result[0].insertId,
     ...userData,
