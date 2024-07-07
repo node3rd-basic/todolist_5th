87c03fd9215ccfc2718993a6d87abdfbc612685a
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-07-07 17:21:50 +0900

    feat:prisma

--- homework/najiyoon/backend/repository/todoitems.repository.js
+++ homework/najiyoon/backend/repository/todoitems.repository.js
@@ -1,49 +1,46 @@
-import conn from "../common/conn.js";
+import prisma from "../common/prisma.js";
+import todoitems from "../db/todoitems.js";
+// import conn from "../common/conn.js";
 
 //목록조회
-export async function findMany(user_id) {
-  // console.log(user_id);
-  const sql = `select * from todo_items where user_id = ?`;
-  const [rows] = await conn.execute(sql, [user_id]);
-
-  const result = rows.map((row) => {
-    return {
-      id: row.id,
-      userId: row.user_id,
-      title: row.title,
-      doneAt: row.done_at,
-      createdAt: row.created_at,
-      updatedAt: row.updated_at,
-    };
-  });
-  return result;
+export async function findMany(userId) {
+  return await prisma.TodoItem.findMany({ where: { userId: +userId } });
 }
-
 export async function saveTodoItem(todoItem) {
-  console.log(todoItem);
-  const sql = `insert into todo_items (user_id, title) values (:user_id, :title)`;
-  const [result] = await conn.execute(sql, todoItem);
-  // console.log("result", result);
-  return result.insertId;
+  // console.log("TodoItem", TodoItem);
+  console.log("todoItem->", todoItem);
+  return await prisma.TodoItem.create({
+    data: { userId: todoItem.userId, title: todoItem.title },
+    select: {
+      id: true,
+      userId: true,
+      title: true,
+      doneAt: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
 }
 //이거먼저 1개 조회 : 수정이 해당 목록찾는거랑 같으니 써도 되지않으까
 export async function oneTodoItem(id) {
-  const sql = `select * from todo_items where id = ?`;
-  const [todoItem] = await conn.execute(sql, [id]);
-  return todoItem[0] || null;
+  console.log("Id---", id);
+  return await prisma.TodoItem.findUnique({ where: { id: id } });
 }
 
-export async function update(id) {
+export async function update(id, doneAt) {
   console.log("이건 id", id);
-  // const sql = `update todo_items set done_at = NOW() WHERE id = ${todoitems.id}`;
+  console.log("doneAt", doneAt);
+  return await prisma.TodoItem.update({ where: { id: +id }, data: { doneAt } });
 
-  const sql = `update todo_items set done_at = if(done_at is null, now(), null) where id = ?`;
-  await conn.execute(sql, [id]);
+  // // const sql = `update todo_items set done_at = NOW() WHERE id = ${todoitems.id}`;
+  // const sql = `update todo_items set done_at = if(done_at is null, now(), null) where id = ?`;
+  // await conn.execute(sql, [id]);
 }
 
 //삭제할 인덱스 : 서비스에서 정의한 아이디
 export async function deleteItem(id) {
   console.log("id->", id);
-  const sql = `delete from todo_items where id = ?`;
-  const [rows] = await conn.execute(sql, [id]);
+  return await prisma.TodoItem.delete({ where: { id: +id } });
+  // const sql = `delete from todo_items where id = ?`;
+  // const [rows] = await conn.execute(sql, [id]);
 }
--- homework/najiyoon/backend/repository/users.repository.js
+++ homework/najiyoon/backend/repository/users.repository.js
@@ -1,37 +1,30 @@
-import conn from "../common/conn.js";
-//헷갈리니 data
+import prisma from "../common/prisma.js";
 
-//기존 자체 디비사용했을 때
-
+//이미 가입한 회원인지
 export async function findUserByEmail(email) {
-  const [users] = await conn.execute(`SELECT * FROM users WHERE email = ?`, [
-    email,
-  ]);
-
-  // return data;
-  return users[0];
+  return await prisma.User.findFirst({
+    where: {
+      email,
+    },
+  });
 }
-
+//회원가입
 export async function postNewUser(email, role, name, password) {
-  const sql = `INSERT INTO users (email, role, name, password) VALUES (:email, :role, :name, :password)`;
-  console.log("sql->", sql);
-  const result = await conn.execute(sql, { email, role, name, password });
-  console.log("result->", result);
-
-  return {
-    id: result[0].insertId,
-    email,
-    role,
-    name,
-  };
+  const newUser = await prisma.User.create({
+    data: {
+      email: email,
+      role: role,
+      name: name,
+      password: password,
+    },
+  });
+  return newUser;
 }
 
 //로그인//
 export async function postUser(email, password) {
-  const sql = `select * from users where (email = ? AND password = ?)`;
-  const [users] = await conn.execute(sql, [email, password]);
-
-  console.log("users--->", users);
-  // findfirst
-  return users[0];
+  const signUser = prisma.user.findFirst({
+    where: { email, password },
+  });
+  return signUser;
 }
