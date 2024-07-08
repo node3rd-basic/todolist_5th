c92c4cb53138209278710a6054e08caeede4c16d
GinGer-JSK <jskim5701@gmail.com>
2024-07-09 00:17:23 +0900

    RAW Query
    모든 코드 raw Query 로 변경:

--- homework/jinseo_kim/backend/controllers/todoItems.controller.js
+++ homework/jinseo_kim/backend/controllers/todoItems.controller.js
@@ -2,27 +2,33 @@ import * as todoItemService from '../services/todoItem.service.js';
 import * as validate from '../validatetor/todoitemid.validatetor.js';
 
 
-export function getTodoItems(req, res) {
+export async function getTodoItems(req, res) {
   const user = req.user;
-  const todoItems = todoItemService.getTodoItemsByUserId(user.id);
+  const todoItems = await todoItemService.getTodoItemsByUserId(user.id);
   res.send(todoItems);
 }
 
-export function postTodoItem(req, res) {
+export async function getTodoItem(req, res) {
+  const {id} = req.params;
+  const todoItems = await todoItemService.getTodoItemsById(id);
+  res.send(todoItems);
+}
+
+export async function postTodoItem(req, res) {
   const { title } = req.body;
   const user = req.user;
-  const newTodoItem = todoItemService.createTodoItem(title, user.id);
+  const newTodoItem = await todoItemService.createTodoItem(title, user.id);
   res.send(newTodoItem);
 }
 
-export function deleteTodoItem(req, res) {
+export async function deleteTodoItem(req, res) {
   const id = validate.validateTodoItemId(req);
-  todoItemService.deleteTodoItemById(id);
+  await todoItemService.deleteTodoItemById(id);
   res.send({ result: 'true' });
 }
 
-export function putTodoItem(req, res) {
+export async function putTodoItem(req, res) {
   const id = validate.validateTodoItemId(req);
-  todoItemService.toggleDonAtById(id);
+  await todoItemService.toggleDonAtById(id);
   res.send({ result: 'true' });
 }
