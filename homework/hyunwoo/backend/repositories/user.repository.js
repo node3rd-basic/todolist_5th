af06076cf8e65aceb4cfa135bbebd893ec30697a
benefrihw <gjtja7@naver.com>
2024-07-09 02:55:24 +0900

    user prims 리팩토링

--- homework/hyunwoo/backend/repositories/user.repository.js
+++ homework/hyunwoo/backend/repositories/user.repository.js
@@ -1,23 +1,26 @@
 // import usersDB from "../db/users.js";
 import conn from "../common/conn.js";
-import prisma from '../common/prisma.js';
+import prisma from "../common/prisma.js";
 
 // user email 찾기
 export async function findOne(email) {
   const user = await prisma.users.findFirst({
     where: {
-      email
-    }
-  })
+      email,
+    },
+  });
   return user;
 }
 
 // user 저장하기
 export async function createUser(userData) {
-  const sql = `INSERT INTO users (email, password, name, role) VALUES ("${userData.email}", "${userData.password}", "${userData.name}", "${userData.role}")`;
-  const result = await conn.execute(sql);
-  return {
-    id: result[0].insertId,
-    ...userData,
-  };
+  const newUser = await prisma.users.create({
+    data: {
+      email: userData.email,
+      password: userData.password,
+      role: userData.role,
+      name: userData.name,
+    },
+  });
+  return newUser;
 }
